343e0d2025f38cb381bbde74b2b2f652e510ad8e
zz106603 <629jyh7@gmail.com>
2020-11-25 19:26:18 +0900

    CRUD구현 및 (목록,글쓰기) VIEW구현

---
+++ spring-todo/todolist/src/main/java/todo/todolist/constant/Method.java
@@ -0,0 +1,6 @@
+package todo.todolist.constant;
+
+public enum Method {
+
+    GET, POST, PUT, PATCH, DELETE
+}
--- spring-todo/todolist/src/main/java/todo/todolist/controller/TodoController.java
+++ spring-todo/todolist/src/main/java/todo/todolist/controller/TodoController.java
@@ -9,15 +9,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import todo.todolist.constant.Method;
 import todo.todolist.domain.TodoDTO;
 import todo.todolist.service.TodoService;
+import todo.todolist.util.UiUtils;
 
 import javax.persistence.EntityManager;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-public class TodoController {
+public class TodoController extends UiUtils{
 
     private TodoService todoService;
 
@@ -25,6 +27,9 @@ public class TodoController {
         this.todoService = todoService;
     }
 
+    /*
+    글 작성
+     */
     @GetMapping(value = "todo/write.do")
     public String openTodoWrite(@RequestParam(value = "idx", required = false) Long idx, Model model){
 
@@ -44,23 +49,28 @@ public class TodoController {
         return "todo/write";
     }
 
+    /*
+    글 등록
+     */
     @PostMapping(value = "todo/register.do")
-    public String register(final TodoDTO todoDTO){
-
+    public String register(final TodoDTO todoDTO, Model model){
         try {
             boolean isRegistered = todoService.registerTodo(todoDTO);
             if (isRegistered == false) {
-                System.out.println("실패");
-                //실패
+                return showMessageWithRedirect("게시글 등록에 실패하였습니다.", "/todo/list.do", Method.GET, null, model);
             }
         }catch (DataAccessException e){
-            System.out.println("db문제");
+            return showMessageWithRedirect("데이터베이스 문제가 발생하였습니다.", "/todo/list.do", Method.GET, null, model);
         }catch (Exception e){
-            System.out.println("시스템문제");
+            return showMessageWithRedirect("시스템 문제가 발생하였습니다.", "/todo/list.do", Method.GET, null, model);
+
         }
-        return "redirect:/todo/list.do";
+        return showMessageWithRedirect("게시글 등록이 완료되었습니다.", "/todo/list.do", Method.GET, null, model);
     }
 
+    /*
+    글 목록
+     */
     @GetMapping(value = "todo/list.do")
     public String openTodoList(Model model){
 
@@ -68,6 +78,50 @@ public class TodoController {
         model.addAttribute("list", list);
 
         return "todo/list";
+    }
+
+    /*
+    글 상세정보
+     */
+    @GetMapping(value = "todo/view.do")
+    public String openTodoDetail(@RequestParam(value = "idx", required = false) Long idx, Model model){
+        if(idx == null){
+            //올바르지 않은 접근
+            return "redirect:/todo/list.do";
+        }
+
+        TodoDTO todoDTO = todoService.getTodoDetail(idx);
+        if(todoDTO == null || todoDTO.getDeleteYn().equals("Y")){
+            //없거나 삭제
+            return "redirect:/todo/list.do";
+        }
+        model.addAttribute("todo", todoDTO);
+
+        return "todo/view";
+    }
+
+    /*
+    글 삭제
+     */
+    @PostMapping(value = "todo/delete.do")
+    public String deleteTodo(@RequestParam(value = "idx", required = false)Long idx, Model model){
+        if(idx == null){
+            //올바르지 않은 접근
+            return "redirect:/todo/list.do";
+        }
+
+        try {
+            boolean isDelete = todoService.deleteTodo(idx);
+            if(isDelete == false){
+                return showMessageWithRedirect("게시글 삭제에 실패하였습니다.", "/todo/list.do", Method.GET, null, model);
+            }
+        }catch (DataAccessException e){
+            return showMessageWithRedirect("데이터베이스 문제가 발생하였습니다.", "/todo/list.do", Method.GET, null, model);
+        }catch (Exception e){
+            return showMessageWithRedirect("시스템에 문제가 발생하였습니다.", "/todo/list.do", Method.GET, null, model);
+        }
+
+        return showMessageWithRedirect("게시글 삭제가 완료되었습니다.", "/todo/list.do", Method.GET, null, model);
     }
 
 }
